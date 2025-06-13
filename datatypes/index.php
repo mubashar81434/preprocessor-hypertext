119485ee3a966c54328bd8fbe4d97413888a3ab3
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-13 07:05:07 +0500

    adding datatypes/

---
+++ datatypes/index.php
@@ -0,0 +1,9 @@
+<!-- We dont need to define the data type of a variable explicitely -->
+<!-- php itself assign the data type of variable based on the value in a variable -->
+<?php
+    $myName = "alpha"; // string
+    $myAge = 23; // int
+    $isAdult = true; //boolean
+    $favTechnologies = ["javaScript", "php", "Expressjs", "Nodejs","Tailwind", "Cloudinary" ]; // array
+    $salary = 16.00; // float
+    
