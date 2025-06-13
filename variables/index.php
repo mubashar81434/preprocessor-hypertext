ec162608a41cf807ffd71018ce980be9b6731781
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-13 06:24:58 +0500

    adding variables/

---
+++ variables/index.php
@@ -0,0 +1,10 @@
+<!-- Variables are used to store data -->
+<!-- Variable name can only start with a letter or underscore-->
+<!-- variable name cant be start wit a digit -->
+<!-- variable names are case sensitive -->
+
+<?php
+    $name = "alpha";
+    $age = 23;
+    echo "My name is $name and i m $age years old";
+    
